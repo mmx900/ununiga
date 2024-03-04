37ee553c081cd2f65eb61c7e9b5ab284fc0c4549
Soyu Kim <mmx900@gmail.com>
2024-03-04 10:18:03 +0900

    WIP HTML 판단에 보다 자연스러운 메서드 활용

--- lib/ununiga/i18n/josa_transformer.rb
+++ lib/ununiga/i18n/josa_transformer.rb
@@ -3,6 +3,10 @@ require 'ununiga/josa_picker'
 module Ununiga::I18n
   module JosaTransformer
     def translate(locale, key, options = {})
+      # 사실 html 파악은 rails 쪽의 기능이다.
+      # https://github.com/rails/rails/blob/6f0d1ad14b92b9f5906e44740fce8b4f1c7075dc/actionview/lib/action_view/helpers/translation_helper.rb#L73
+      # 문제는, html 키인지 판단하는 메서드는 private으로 되어 있다.
+      # https://github.com/rails/rails/blob/6f0d1ad14b92b9f5906e44740fce8b4f1c7075dc/activesupport/lib/active_support/html_safe_translation.rb
       transform(super, key.end_with?("_html", ".html"), locale)
     end
 
